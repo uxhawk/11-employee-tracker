602360a41b24b01c7966de42d048fd271ac1efd3
David Hawkins <uxhawk@gmail.com>
2020-04-23 18:00:44 -0400

    server.js asks initial prompt and connects to employees_db

--- server.js
+++ server.js
@@ -1,2 +1,52 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employees_db"
+});
+
+connection.connect(err => {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    console.log("Successfully connected");
+    actionPrompt();
+});
+
+function actionPrompt() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "Add content",
+            "View content",
+            "Edit content",
+            "Exit"
+        ]
+    }).then(answer => {
+        switch (answer.action) {
+            case "Add content":
+                addPrompt();
+                break;
+
+            case "View content":
+                viewPrompt();
+                break;
+
+            case "Edit content":
+                editPrompt();
+                break;
+
+            case "Exit":
+            default:
+                connection.end();
+                break;
+        }
+    });
+}
