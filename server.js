a9fa92e7002fcac58dd21a6d5e27686cc2ef9a0b
David Hawkins <uxhawk@gmail.com>
2020-04-26 21:24:27 -0400

    added module requirements to each class

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ const manager = new Manager();
 const department = new Department();
 const Role = new Role();
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "employees_db"
-});
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "",
+//     database: "employees_db"
+// });
 
 connection.connect(err => {
     if (err) {
